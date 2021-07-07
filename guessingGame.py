ed0179eca3f71092aca8bc73c6c95907fee5b6ac
advaitsharma <sharmaadvait2@gmail.com>
2021-07-07 16:04:38 +0530

    added files

---
+++ guessingGame.py
@@ -0,0 +1,14 @@
+import random
+
+guessedNumber = int(input("Guess the number (You have 5 chances): "))
+
+n = random.randint(1, 9)
+
+chances = 0
+
+if(guessedNumber == n):
+    print("You guessed the correct number!")
+else:
+    chances = chances-1
+    print("Wrong Number. TRY AGAIN!")
+    print"You have", chances, "chances"
